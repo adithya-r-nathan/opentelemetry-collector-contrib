4547e8cd7f8620e9a2216edf46a4dcf13c029b70
adithya-r-nathan <adithya.r.nathan@gmail.com>
2026-02-24 08:40:51 +0530

    Fix Lint

--- extension/encoding/awslogsencodingextension/internal/unmarshaler/tgw-flow-log/unmarshaler_test.go
+++ extension/encoding/awslogsencodingextension/internal/unmarshaler/tgw-flow-log/unmarshaler_test.go
@@ -1,5 +1,4 @@
 // Copyright The OpenTelemetry Authors
-
 // SPDX-License-Identifier: Apache-2.0
 
 package tgwflowlog
@@ -23,37 +22,26 @@ import (
 
 func compressToGZIPReader(t *testing.T, buf []byte) io.Reader {
 	var compressedData bytes.Buffer
-
 	gzipWriter := gzip.NewWriter(&compressedData)
-
 	_, err := gzipWriter.Write(buf)
-
 	require.NoError(t, err)
-
 	err = gzipWriter.Close()
-
 	require.NoError(t, err)
 
 	gzipReader, err := gzip.NewReader(bytes.NewReader(compressedData.Bytes()))
-
 	require.NoError(t, err)
-
 	return gzipReader
 }
 
 func readAndCompressLogFile(t *testing.T, dir, file string) io.Reader {
 	data, err := os.ReadFile(filepath.Join(dir, file))
-
 	require.NoError(t, err)
-
 	return compressToGZIPReader(t, data)
 }
 
 func readLogFile(t *testing.T, dir, file string) io.Reader {
 	data, err := os.ReadFile(filepath.Join(dir, file))
-
 	require.NoError(t, err)
-
 	return bytes.NewReader(data)
 }
 
@@ -63,43 +51,29 @@ func TestUnmarshalLogs_PlainText(t *testing.T) {
 	dir := "testdata"
 
 	tests := []struct {
-		name string
-
-		logInputReader io.Reader
-
+		name                 string
+		logInputReader       io.Reader
 		logsExpectedFilename string
-
-		expectedErr string
+		expectedErr          string
 	}{
 		{
-			name: "Valid TGW flow log - single log",
-
-			logInputReader: readAndCompressLogFile(t, dir, "valid_tgw_flow_log_single.log"),
-
+			name:                 "Valid TGW flow log - single log",
+			logInputReader:       readAndCompressLogFile(t, dir, "valid_tgw_flow_log_single.log"),
 			logsExpectedFilename: "valid_tgw_flow_log_single_expected.yaml",
 		},
-
 		{
-			name: "Valid TGW flow log - multi logs",
-
-			logInputReader: readAndCompressLogFile(t, dir, "valid_tgw_flow_log_multi.log"),
-
+			name:                 "Valid TGW flow log - multi logs",
+			logInputReader:       readAndCompressLogFile(t, dir, "valid_tgw_flow_log_multi.log"),
 			logsExpectedFilename: "valid_tgw_flow_log_multi_expected.yaml",
 		},
-
 		{
-			name: "Valid TGW flow log uncompressed",
-
-			logInputReader: readLogFile(t, dir, "valid_tgw_flow_log_single.log"),
-
+			name:                 "Valid TGW flow log uncompressed",
+			logInputReader:       readLogFile(t, dir, "valid_tgw_flow_log_single.log"),
 			logsExpectedFilename: "valid_tgw_flow_log_single_expected.yaml",
 		},
-
 		{
-			name: "Empty input",
-
-			logInputReader: bytes.NewReader([]byte{}),
-
+			name:                 "Empty input",
+			logInputReader:       bytes.NewReader([]byte{}),
 			logsExpectedFilename: "valid_tgw_flow_log_empty_expected.yaml",
 		},
 	}
@@ -107,34 +81,25 @@ func TestUnmarshalLogs_PlainText(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			u, err := NewTGWFlowLogUnmarshaler(
-
 				Config{FileFormat: constants.FileFormatPlainText},
-
 				component.BuildInfo{},
-
 				zap.NewNop(),
 			)
-
 			require.NoError(t, err)
 
 			logs, err := u.UnmarshalAWSLogs(test.logInputReader)
 
 			if test.expectedErr != "" {
-
 				require.ErrorContains(t, err, test.expectedErr)
-
 				return
-
 			}
 
 			// To generate golden files, uncomment:
-
 			// golden.WriteLogsToFile(filepath.Join(dir, test.logsExpectedFilename), logs)
 
 			require.NoError(t, err)
 
 			expectedLogs, err := golden.ReadLogs(filepath.Join(dir, test.logsExpectedFilename))
-
 			require.NoError(t, err)
 
 			require.NoError(t, plogtest.CompareLogs(expectedLogs, logs))
